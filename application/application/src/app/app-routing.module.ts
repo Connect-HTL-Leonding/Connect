bc3584ba3698dcae0de52f91d94c78a7e8f8e9b3
Rafetseder Tobias <tobias.rafetseder@gmx.at>
2020-12-18 09:19:30 +0100

    Profile Page generated

--- application/application/src/app/app-routing.module.ts
+++ application/application/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   },
+
 ];
 
 @NgModule({
