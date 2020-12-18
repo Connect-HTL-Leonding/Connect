8d7483c22d46591a8e8ae1aa913388736c47b96c
TristanHill <t.hill@students.htl-leonding.ac.at>
2020-12-18 09:34:18 +0100

    Return Routing

---
+++ application/application/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },{
+    path: 'skinselection',
+    loadChildren: () => import('./skinselection/skinselection.module').then( m => m.SkinselectionPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'my-skins',
+    loadChildren: () => import('./my-skins/my-skins.module').then( m => m.MySkinsPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
