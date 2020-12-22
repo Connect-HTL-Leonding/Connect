415835e4926afe21336cf064da55f6c6fa1a5b45
Rafetseder Tobias <tobias.rafetseder@gmx.at>
2020-12-22 13:30:22 +0100

    added slider options

--- application/application/src/app/profile/profile.page.ts
+++ application/application/src/app/profile/profile.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ProfileService} from "../api/profile.service";
-import {User} from "../model/user";
+import { ProfileService } from "../api/profile.service";
+import { User } from "../model/user";
 
 @Component({
   selector: 'app-profile',
@@ -12,10 +12,17 @@ export class ProfilePage implements OnInit {
   user: User
 
   constructor(ps: ProfileService) {
-      this.user = ps.user[0];
-   }
+    this.user = ps.user[0];
+  }
 
-   
+  slideOpts = {
+    //initialSlide: 0,
+   // spaceBetween: "-550",
+    slidesPerView: 1,
+    speed: 400
+  };
+
+
 
   ngOnInit() {
 
