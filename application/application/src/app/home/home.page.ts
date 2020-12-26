8e5d86db16e2346120c902547ca04e5b74df85ce
TristanHill <t.hill@students.htl-leonding.ac.at>
2020-12-26 16:18:01 +0100

    Implemented Google Maps. Must be started with 'ionic cordova run browser'

--- application/application/src/app/home/home.page.ts
+++ application/application/src/app/home/home.page.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +17,52 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  map: GoogleMap;
   navigate:any
 
   constructor(private menu : MenuController) {
     this.sideMenu();
   }
 
+  async ngOnInit(){
+    this.loadMap();
+  }
+
+loadMap() {
+ 
+  // This code is necessary for browser
+  Environment.setEnv({
+    'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBtsGRD6cmDQvehofWlUINq1SwvSK-Iq_Q',
+    'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBtsGRD6cmDQvehofWlUINq1SwvSK-Iq_Q'
+  });
+
+  let mapOptions: GoogleMapOptions = {
+    camera: {
+       target: {
+         lat: 43.0741904,
+         lng: -89.3809802
+       },
+       zoom: 18,
+       tilt: 30
+     }
+  };
+
+  this.map = GoogleMaps.create('map_canvas', mapOptions);
+/*
+  let marker: Marker = this.map.addMarkerSync({
+    title: 'Ionic',
+    icon: 'blue',
+    animation: 'DROP',
+    position: {
+      lat: 43.0741904,
+      lng: -89.3809802
+    }
+  });
+  marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+    alert('clicked');
+  });
+  */
+}
 
   sideMenu()
   {
