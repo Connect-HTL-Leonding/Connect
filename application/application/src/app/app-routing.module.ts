05c149175f4f9c27dcf0a547e10bbacb8224c52b
simonibrahim <s.ibrahim@students.htl-leonding.ac.at>
2021-01-04 17:05:01 +0100

    generated chat page

--- application/application/src/app/app-routing.module.ts
+++ application/application/src/app/app-routing.module.ts
@@ -28,6 +28,10 @@ const routes: Routes = [
   },  {
     path: 'edit-profile',
     loadChildren: () => import('./edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
+  },
+  {
+    path: 'chat',
+    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
   }
 
 ];
