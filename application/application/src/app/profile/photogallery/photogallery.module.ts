2e7b1d2a71bb3fdabfa91b6947fceb0aa12ec031
RafetsederTobias <tobias.rafetseder@gmx.at>
2021-02-05 09:44:18 +0100

    added a hard-coded photogallery with photoviewer

--- application/application/src/app/profile/photogallery/photogallery.module.ts
+++ application/application/src/app/profile/photogallery/photogallery.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
 import { IonicModule } from '@ionic/angular';
 
 import { PhotogalleryPageRoutingModule } from './photogallery-routing.module';
