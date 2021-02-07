d312dce05ebae2e9264c3a601414a209f349b406
Jan Donnerbauer <donnerbauerjan@gmail.com>
2021-02-07 16:18:03 +0100

    Changed User

--- application/application/src/app/edit-profile-details/edit-profile-details.page.ts
+++ application/application/src/app/edit-profile-details/edit-profile-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../api/auth/auth.service';
 import { ProfileService } from '../api/profile.service';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-edit-profile-details',
@@ -12,10 +13,38 @@ export class EditProfileDetailsPage implements OnInit {
   constructor(public ps: ProfileService, private auth: AuthService) { }
 
   ngOnInit() {
+    this.ps.getUser().subscribe(
+      data => {
+
+        console.log(data);
+        this.ps.user = data;
+
+        console.log(this.ps.user)
+        //console.log(this.skinService);
+      },
+      error1 => {
+        console.log('Error');
+      }
+    )
   }
 
   logout() { 
     this.auth.logout(); 
+  }
+
+  updateUser(u: User) {
+    //u.username = document.getElementById("username").innerText;
+    //console.log(u.desc)
+    //u.desc = document.getElementById("desc").innerText;
+    //console.log(u.desc)
+
+    console.log(u)
+
+    this.ps.updateUser(u).subscribe(data => {
+      //nach unpdate erneutes getAll
+      console.log(data)
+      this.ps.user = data;
+    });
   }
 
 }
--- application/application/src/app/model/user.ts
+++ application/application/src/app/model/user.ts
@@ -26,19 +26,11 @@ export class User {
     public id = 0,
     public username: string = "",
     public profilePicture: string = "",
-    public desc: string = "",
-    public instagram : string = "",
-    public facebook: string = "",
-    public twitter: string = "",
-    public linkedIn : string = "",
+    public description: string = "",
     public gender: string = "",
-    public questions = {
-        q1: "",
-        q2: "",
-        q3: ""
-    },
-    public password: string  ="",
-    public email: string = ""
+    public email: string = "",
+    public birthday : Date = new Date(),
+    public fullname : string = ""
 ) {
 
 }
