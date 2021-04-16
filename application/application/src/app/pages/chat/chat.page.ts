7582e079d76b837278016c38ca5010f83f662d33
RafetsederTobias <tobias.rafetseder@gmx.at>
2021-04-16 09:49:59 +0200

    small bug fix

--- application/application/src/app/pages/chat/chat.page.ts
+++ application/application/src/app/pages/chat/chat.page.ts
@@ -187,8 +187,6 @@ export class ChatPage implements OnInit {
         console.log(this.unseenMessages + " unseen")
         this.chatservice.getData().subscribe(data => {
           this.chatservice.messages = data;
-          console.log(this.convert(this.chatservice.messages[0].created));
-          console.log(this.chatservice.messages.length);
           this.pos = this.chatservice.messages.length - this.unseenMessages;
           
         })
@@ -199,6 +197,9 @@ export class ChatPage implements OnInit {
 
    newDay(message : Message, olderMessage : Message) : boolean {
      let newDay = false;
+     if(message==undefined) {
+       return false;
+     }
      if(olderMessage==undefined) {
        return true;
      }
