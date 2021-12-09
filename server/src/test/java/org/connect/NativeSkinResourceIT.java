7b1001a64423253339580327bf1fcb92d780b8fe
Jan Donnerbauer <donnerbauerjan@gmail.com>
2021-12-09 16:38:43 +0100

    added Regis Flow

--- server/src/test/java/org/connect/NativeSkinResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.connect;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeSkinResourceIT extends SkinResourceTest {
-
-    // Execute the same tests but in native mode.
-}
