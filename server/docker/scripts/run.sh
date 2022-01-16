13873266d29f9aba934a4816b337b9f43740e1e8
Jan Donnerbauer <donnerbauerjan@gmail.com>
2022-01-16 17:55:41 +0100

    added curl

--- server/docker/scripts/run.sh
+++ server/docker/scripts/run.sh
@@ -2,7 +2,7 @@
 
 RUNNER=$(ls *-runner.jar)
 
-while ! nc -z db_connect 3306 && ! nc -z keycloak 8080; do
+while ! nc -z db_connect 3306 && ! curl --output /dev/null --silent --head --fail "http://keycloak:8080/auth"; do
     echo "waiting for godot..."
     nc -v keycloak 8080
     sleep 1
