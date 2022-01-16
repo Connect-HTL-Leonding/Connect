3dc862d125074b4bffb1ea547ed88870cc8b96b9
Jan Donnerbauer <donnerbauerjan@gmail.com>
2022-01-16 18:19:10 +0100

    logikfehler gelöst

--- server/docker/scripts/run.sh
+++ server/docker/scripts/run.sh
@@ -6,7 +6,7 @@ if ! curl --output /dev/null --silent --head --fail "http://keycloak:8080/auth";
   echo "test"
 fi
 
-while ! nc -z db_connect 3306 && ! curl --output /dev/null --silent --head --fail "http://keycloak:8080/auth"; do
+while ! nc -z db_connect 3306 || ! curl --output /dev/null --silent --head --fail "http://keycloak:8080/auth"; do
     echo "waiting for godot..."
     nc -v keycloak 8080
     sleep 1
